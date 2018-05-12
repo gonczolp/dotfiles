b5e6b93ce4a6e4de7cb0f04e1463a609d48c28ad
Peter Gönczöl <gonczolp@gmail.com>
2018-05-12 12:44:18 +0200

    Added new screenlayouts

--- .screenlayout/extend-right.sh
+++ .screenlayout/extend-right.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
+killall compton
 xrandr --output VIRTUAL1 --off --output DP3 --off --output DP2 --off --output DP1 --off --output HDMI3 --mode 1920x1080 --pos 1600x0 --rotate normal --output HDMI2 --off --output HDMI1 --off --output LVDS1 --primary --mode 1600x900 --pos 0x0 --rotate normal --output VGA1 --off
+compton -b
+i3-msg restart
